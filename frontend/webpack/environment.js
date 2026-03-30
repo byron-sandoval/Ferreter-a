5b6e0a5ce1e4f891907e4bd7800a382ffa2e7c01
byron-sandoval <dg1109600@gmail.com>
2026-03-29 23:37:20 -0600

    build(frontend): modificar package y webpack para enlazar dependencias de pasarela

--- frontend/webpack/environment.js
+++ frontend/webpack/environment.js
@@ -1,3 +1,15 @@
+const fs = require('fs');
+const path = require('path');
+const dotenv = require('dotenv');
+
+// Cargar variables del archivo .env raíz del frontend
+const envPath = path.resolve(__dirname, '../.env');
+let parsedEnv = {};
+if (fs.existsSync(envPath)) {
+  parsedEnv = dotenv.parse(fs.readFileSync(envPath));
+}
+dotenv.config({ path: envPath, override: true });
+
 module.exports = {
   // APP_VERSION is passed as an environment variable from the Gradle / Maven build tasks.
   VERSION: process.env.APP_VERSION || 'DEV',
@@ -6,4 +18,6 @@ module.exports = {
   // If you use an API server, in `prod` mode, you will need to enable CORS
   // (see the `jhipster.cors` common JHipster property in the `application-*.yml` configurations)
   SERVER_API_URL: '',
+  // Stripe: clave pública cargada desde el .env del frontend (priorizando el archivo sobre el OS)
+  STRIPE_PUBLISHABLE_KEY: parsedEnv.REACT_APP_STRIPE_PUBLISHABLE_KEY || process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY || '',
 };
